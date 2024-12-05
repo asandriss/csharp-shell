b2e118263a503a49a198f6dcbd62bcc020ef6975
Fedor Hajdu <fedorhajdu@gmail.com>
2024-12-05 08:50:12 +0000

    Allow type to fine exec files

--- src/TypeCommand.cs
+++ src/TypeCommand.cs
@@ -6,15 +6,51 @@ public class TypeCommand(Dictionary<string, IShellCommand> registry) : IShellCom
     {
         var type = args.FirstOrDefault();
 
-        var result = registry.ContainsKey(type) ?
-            $"{type} is a shell builtin" 
-            : $"{type} not found";
+        string result;
+        if (registry.ContainsKey(type))
+            return $"{type} is a shell builtin" + Environment.NewLine;
+        var exec = GetExecutableProgram(type);
 
-        return result + Environment.NewLine;
+        if (!string.IsNullOrWhiteSpace(exec))
+        {
+            return $"{type} is {exec}" + Environment.NewLine;
+        }
+
+        return $"{type}: not found" + Environment.NewLine;
     }
 
     public bool ValidateArguments(IEnumerable<string>? args)
     {
         return args is not null && args.Any();
+    }
+
+    private static string GetExecutableProgram(string name)
+    {
+        foreach (var dir in GetPathLocations())
+        {
+            if (string.IsNullOrWhiteSpace(dir)) continue;
+            if (!Directory.Exists(dir)) continue;
+            
+            foreach (var file in Directory.GetFiles(dir))
+            {
+                var lastPart = file.Split('/').LastOrDefault();
+                if (!string.IsNullOrWhiteSpace(lastPart) && lastPart == name)
+                {
+                    return file;
+                }
+            }
+        }
+        return String.Empty;
+    }
+
+    private static IEnumerable<string> GetPathLocations()
+    {
+        var pathStr = Environment.GetEnvironmentVariable("PATH") ?? "";
+        var allPaths = pathStr.Split(':');
+
+        foreach (var path in allPaths)
+        {
+            yield return path;
+        }
     }
 }
